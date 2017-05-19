2dc8eb37628a4ea3e44a6c1a15fd357a8c9bcd8d
JounQin <liujiaren1992@vip.qq.com>
2017-05-19 11:36:06 +0800

    eslint-plugin-prettier options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   parser: 'babel-eslint',
   plugins: ['prettier'],
-  extends: ['prettier']
+  extends: ['prettier'],
+  rules: {
+    'prettier/prettier': [2, {
+      bracketSpacing: false,
+      semi: false,
+      singleQuote: true
+    }]
+  }
 }
--- src/vue/entry.js
+++ src/vue/entry.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 import router from 'router'
 
 new Vue({
-  render(){
-    return <div id="app">
-      <router-view/>
-    </div>
+  render() {
+    return (
+      <div id="app">
+        <router-view />
+      </div>
+    )
   },
   router
 }).$mount('#app')
